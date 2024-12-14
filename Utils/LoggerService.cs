0ebd899fee26a8d8b29953a857f28b0388f3e931
SweetSmellFox <1254371679@qq.com>
2024-12-14 16:03:34 +0800

    style: 修改日志打印方式

--- Utils/LoggerService.cs
+++ Utils/LoggerService.cs
@@ -10,20 +10,25 @@ public static class LoggerService
     public static void LogInfo(string message)
     {
         Logger.Info(message);
+        Console.WriteLine("[INFO]" + message);
     }
 
     public static void LogError(object? e)
     {
         Logger.Error(e?.ToString() ?? string.Empty);
+
+        Console.WriteLine("[ERROR]" + e);
     }
 
     public static void LogError(string message)
     {
         Logger.Error(message);
+        Console.WriteLine("[ERROR]" + message);
     }
 
     public static void LogWarning(string message)
     {
         Logger.Warn(message);
+        Console.WriteLine("[WARN]" + message);
     }
 }
