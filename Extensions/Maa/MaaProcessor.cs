09bd12bddc5f97f93cd2c5d283c97dce89ea0990
SweetSmellFox <1254371679@qq.com>
2025-03-29 17:35:07 +0800

    feat: agent未完全启动时可以通过停止任务强制关闭agent

--- Extensions/Maa/MaaProcessor.cs
+++ Extensions/Maa/MaaProcessor.cs
@@ -317,6 +317,12 @@ public class MaaProcessor
 
     private void CancelOperations()
     {
+        if (!_agentStarted)
+        {
+            _agentProcess?.Kill();
+            _agentProcess?.Dispose();
+            _agentProcess = null;
+        }
         _emulatorCancellationTokenSource?.SafeCancel();
         CancellationTokenSource.SafeCancel();
     }
